19365ca1842b6a1ddd7f30285bdf8365b8913596
Sibasis-Malla <sibasisml@gmail.com>
2023-04-07 15:49:44 +0530

    feat: signup,login,logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,42 @@ import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import axios from "axios";
+import React,{useState,useEffect} from "react";
 
 export default function App() {
+  axios.defaults.withCredentials = true
+  axios.defaults.baseURL = "http://localhost:8080"
+  const [loggedIn, setloggedIn] = useState(false)
+  const [empID, setuserName] = useState("")
+  const getUser=()=> {
+    axios.get('/').then(response => {
+      console.log('Get user response: ')
+     // console.log(response.data)
+      if (response.data.user) {
+        console.log('Get User: There is a user saved in the server session: ')
+
+      setloggedIn(true);
+      setuserName(response.data.user.name)
+      } else {
+        console.log('Get user: no user');
+        setloggedIn(false);
+        setuserName(null)
+      }
+    })
+  }
+
+    useEffect(()=>{
+      getUser()
+    },[])
   return (
     <>
-      <Navbar />
+      <Navbar isloggedIn={loggedIn} empID={empID} setloggedIn={setloggedIn} setempID={setuserName}/>
       <Routes>
       <Route path="/" element={<Home />}></Route>
-        <Route path="/Login" element={<Login/>}>
-
-        </Route>
+        <Route path="/Login" element={<Login setloggedIn={setloggedIn} setempID={setuserName}/>}/>
+        <Route path="/Signup" element={<Signup/>}/>
       </Routes>
 
       {/* <h1 className="text-3xl font-bold text-purple-700 underline">
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-export default function Navbar() {
+import axios from "axios";
+export default function Navbar(props) {
+
+  const handleLogout = (e)=>{
+      e.preventDefault()
+      console.log('logging out')
+        axios.post('/logout').then(response => {
+          console.log(response.data)
+          if (response.status === 200) {
+            props.setloggedIn(false)
+            props.setempID(null)
+            window.location.href = "/login"
+          }
+        }).catch(error => {
+            console.log('Logout error')
+        })
+      }
+  
   return (
+  
     <>
       <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
@@ -12,22 +30,17 @@ export default function Navbar() {
               class="h-6 mr-3 sm:h-9"
               alt="Flowbite Logo"
             />
-            <Link class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">TMS</Link>
+            <Link to="/" class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">TMS</Link>
            </div>
             {/* <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               Flowbite
             </span> */}
-          {/* </a> */}
           <div class="flex items-center">
-            <a
-              href="tel:5541251234"
-              class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline"
-            >
-              Signup
-            </a>
-            {/* <a class="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"> */}
-              <Link to="/Login" class="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"> Login</Link>
-            {/* </a> */}
+            {props.isloggedIn ?(<>Hey {props.empID} <button onClick={handleLogout} class="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Logout</button></>):(<><Link to="/Signup" class="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"> Signup</Link>
+   
+   <Link to="/Login" class="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"> Login</Link></>)}
+          
+      
           </div>
         </div>
       </nav>
